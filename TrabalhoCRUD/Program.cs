517030d90f7846e54670377de684117c31641549
Jociane Costa E Silva <jociane.silva@hbsis.com.br>
2020-08-20 13:18:37 -0300

    Add project files.

---
+++ TrabalhoCRUD/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrabalhoCRUD.Context;
+using TrabalhoCRUD.Funcoes;
+using TrabalhoCRUD.Menu;
+
+namespace TrabalhoCRUD
+{
+    class Program
+    {
+        static void Main()
+        {
+
+
+            //Inserir.InserirCD();
+            //Inserir.InserirFuncionario();
+           // Inserir.InserirPatrimonio();
+
+
+            Console.ReadKey();
+        }
+    }
+}
